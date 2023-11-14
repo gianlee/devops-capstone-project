2dfa61424d7776523c368cd3a6fadd99986ac66c
gianlee <lgianlucaster@gmail.com>
2023-11-14 09:00:02 -0500

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -13,8 +13,10 @@ from flask_talisman import Talisman
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
-#talisman = flask_talisman(app)
-
+csp = {
+    'default-src': '\'self\''
+}
+talisman = Talisman(app, content_security_policy=csp)
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
--- service/routes.py
+++ service/routes.py
@@ -8,9 +8,6 @@ from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
-from flask_talisman import Talisman
-
-talisman = Talisman(app)
 
 ############################################################
 # Health Endpoint
@@ -27,14 +24,7 @@ def health():
 @app.route("/")
 def index():
     """Root URL response"""
-    return (
-        jsonify(
-            name="Account REST API Service",
-            version="1.0",
-            # paths=url_for("list_accounts", _external=True),
-        ),
-        status.HTTP_200_OK,
-    )
+    return (jsonify(name="Data Retrieval Service", version="1.0"), status.HTTP_200_OK)
 
 
 ######################################################################
