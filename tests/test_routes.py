21b7d0f3e55b3a0ba6fb7e2026f559db128ba48b
gianlee <64149619+gianlee@users.noreply.github.com>
2023-11-16 10:29:20 +0100

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -129,9 +129,7 @@ class TestAccountService(TestCase):
     def test_get_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
-        resp = self.client.get(
-            f"{BASE_URL}/{account.id}", content_type="application/json"
-        , environ_overrides=HTTPS_ENVIRON)
+        resp = self.client.get(f"{BASE_URL}/{account.id}", content_type="application/json", environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
